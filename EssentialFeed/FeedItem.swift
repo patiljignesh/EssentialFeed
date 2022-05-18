a6ec414bcb2630cfef6c529356980f94a3998ee8
Jignesh Patil <patiljignesh@gmail.com>
2022-05-18 08:49:13 +0200

    Create the FeedItem  Model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jigneshkumar Patil on 2022/05/18.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
